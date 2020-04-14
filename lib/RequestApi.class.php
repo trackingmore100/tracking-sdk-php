caa4ed04f3ccdf35ac6cc0cd065a87da8dce6944
trackingmore100 <liushijie@trackingmore.org>
2020-04-14 11:49:48 +0800

    Create for the first time

---
+++ lib/RequestApi.class.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace APITracking;
+
+class RequestApi
+{
+	
+	# @var string The url to be used for requests.
+	public static $url = null;
+	
+	# @var array The params to be used for requests.
+	public static $params = [];
+	
+	# @var array The header to be used for requests.
+	public static $header = [];
+	
+	# @var int The header to be used for requests.
+	public static $timeout = 60;
+	
+	# @var boolean The header to be used for requests.
+	public static $isHeader = false;
+	
+	/**
+	* send api request.
+	*
+	* @return json $response.
+	*/
+	public static function send($method = "GET")
+	{
+		$method = strtoupper($method);
+		if(
+			empty(self::$url) 
+			|| empty(self::$header)
+		) return "";
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, self::$url);
+		if(
+			!empty(self::$params) 
+			&& $method == "GET"
+		) $method = "POST";
+		if(
+			!empty(self::$params) 
+			&& !is_string(self::$params)
+		) self::$params = json_encode(self::$params);
+		switch($method)
+		{
+			case "GET" :
+				curl_setopt($curl, CURLOPT_HTTPGET, true);
+				break;
+			case "POST" :
+				curl_setopt($curl, CURLOPT_POST, true);
+				break;
+			default :
+				curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+				break;
+		}
+		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
+		curl_setopt($curl, CURLOPT_TIMEOUT, self::$timeout);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HEADER, self::$isHeader);
+		if(!empty(self::$params))
+		{
+			curl_setopt($curl, CURLOPT_POSTFIELDS, self::$params);
+			self::$header[] = "Content-Length: " . strlen(self::$params);
+		}
+		curl_setopt($curl, CURLOPT_HTTPHEADER, self::$header);
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+		$response = curl_exec($curl);
+		curl_close($curl);
+		unset($curl); 
+		return $response;
+	}
+	
+}
